572148ea6f62d93eb7e50feac8752599ae4afecc
Tharamac <tharathorn.wang@gmail.com>
2022-05-22 00:32:27 +0700

    add diamond shape and set the box on vertical view

--- lib/pages/vertical_tower.dart
+++ lib/pages/vertical_tower.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:tower_box_test/widget/destory_box_btn.dart';
 import 'package:tower_box_test/widget/towerbox.dart';
+import 'dart:math';
 
 const colors = [Color.fromARGB(255, 216, 154, 174), Colors.blue];
 
@@ -16,14 +17,39 @@ class VerticalTowerView extends StatelessWidget {
         children: [
           Expanded(
               flex: 80,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: List<Widget>.generate(
-                    18,
-                    (i) => (i % 2 == 0)
-                        ? const TowerBox()
-                        : const SizedBox(height: 4)),
-              )),
+              child: Container(
+                  width: double.infinity,
+                  child: SingleChildScrollView(
+                      reverse: true,
+                      // physics: const NeverScrollableScrollPhysics(),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                              Transform(
+                                  alignment: Alignment.center,
+                                  transform: Matrix4.rotationZ(
+                                    pi / 4,
+                                  ),
+                                  child: Container(
+                                      width: 140,
+                                      height: 140,
+                                      decoration: BoxDecoration(
+                                        color: const Color(0xFFD27AFF),
+                                        border: Border.all(
+                                          width: 0.7,
+                                          color: Colors.black,
+                                        ),
+                                      ))),
+                              const SizedBox(
+                                height: 36,
+                              )
+                            ] +
+                            List<Widget>.generate(
+                                18,
+                                (i) => (i % 2 == 0)
+                                    ? const TowerBox()
+                                    : const SizedBox(height: 4)),
+                      )))),
           Expanded(
             flex: 16,
             child: Container(
